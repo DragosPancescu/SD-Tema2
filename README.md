6661291397445529bc0451750f35044ee46a41d2
DragosPancescu <mineralwater19@gmail.com>
2021-12-16 21:44:03 +0200

    Structured the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to Project 2 for the Distributed Systems Laboratory
+
+[![made-with-Go](https://img.shields.io/badge/Made%20with-Go-1f425f.svg)](http://golang.org) [![GitHub go.mod Go version of a Go module](https://img.shields.io/github/go-mod/go-version/gomods/athens.svg)](https://github.com/gomods/athens)
+
+<img src="https://iconape.com/wp-content/png_logo_vector/go-gopher-favicon.png" width="200" height="200" />
---
+++ cerinte.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+Cerinta 1.
+Se dă un vector de vectori, care conțin mai multe cuvinte ce pot avea in componența lor doar litere.
+Să se afle numărul mediu de cuvinte care conțin numar par de vocale si un numar divizibil cu 3 de consoane folosind tehnica map-reduce.
+*/
+func cerinta1(input [][]string) float64 {
+	return 1.1
+}
+
+/*
+Cerinta 2.
+Se dă un vector de vectori, care conțin mai multe cuvinte ce pot avea in componența lor atȃt litere cȃt și cifre.
+Să se afle numărul mediu de cuvinte care sunt palindrom folosind tehnica map-reduce.
+*/
+func cerinta2(input [][]string) float64 {
+	return 1.1
+}
